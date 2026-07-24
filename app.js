9b9d274978888b05c262f1282c22628629f03d1e
tarun kumar <tarunkhedar95@gmail.com>
2026-07-24 06:53:51 +0530

    final project is ready

--- app.js
+++ app.js
@@ -18,13 +18,32 @@ const listingRouter=require("./routes/listing.js");
 const reviewRouter=require("./routes/review.js");
 const cookieParser=require("cookie-parser");
 const session=require("express-session");
+// const MongoStore = require("connect-mongo");
 const flash=require("connect-flash");
 const passport=require("passport");
 const LocalStrategy=require("passport-local");
 const User=require("./models/user.js");
 const userRouter=require("./routes/user.js");
 
+
+// async function main() {
+//   await mongoose.connect(process.env.ATLASDB_URL);
+// }
+
+// const store=MongoStore.create({
+//     mongoUrl:process.env.ATLASDB_URL,
+//     crypto:{
+//         secret:"mysupersecretstring"
+//     },
+//     touchAfter:24*3600,
+// });
+
+// store.on("error",(err)=>{
+//     console.log("Error in session store",err);
+// });
+
 const sessionOptions={
+    // store:store,
     secret:"mysupersecretstring",
     resave:false,
     saveUninitialized:true,
@@ -34,6 +53,8 @@ const sessionOptions={
         httpOnly:true,
     }
 }
+
+
 app.use(session(sessionOptions));
 app.use(flash());
 
@@ -74,14 +95,19 @@ app.get("/verify",(req,res)=>{
     res.send("verified");
 })
 
+
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+}
+
 main().then((res)=>{
     console.log("connected to DB");
 })
 .catch((err) => console.log(err));
 
-async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
-}
+
+
 
 
 app.use("/listings",listingRouter);
